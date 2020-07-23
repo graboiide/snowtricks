7ec5a9134ee1517c4cc524822a8c3700f90989fb
gregory castaing <graboiide@gmail.com>
2020-07-23 20:09:27 +0200

    add readme

---
+++ snowtricks/Install/data.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 23 juil. 2020 à 18:06
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `snowtricks`
+--
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `figure_id`, `user_id`, `date`, `message`) VALUES
+(259, 51, 4, '2020-07-19 16:25:17', 'Fugiat et aut sint eaque voluptatibus et nam qui accusamus omnis qui iste est.'),
+(260, 51, 6, '2020-07-19 16:25:17', 'Eaque labore aut blanditiis nemo veniam voluptatibus quia aut cupiditate.'),
+(261, 51, 9, '2020-07-19 16:25:17', 'Reprehenderit quaerat occaecati cum consectetur numquam amet a repellat.'),
+(262, 51, 9, '2020-07-19 16:25:17', 'Suscipit cupiditate velit quae occaecati qui quam amet iusto dolor.'),
+(263, 52, 3, '2020-07-19 16:25:17', 'Cumque sit repudiandae nulla sit in qui nihil odit non nulla.'),
+(264, 52, 3, '2020-07-19 16:25:17', 'Hic dolores modi sit quas doloremque molestiae accusamus.'),
+(265, 52, 5, '2020-07-19 16:25:17', 'Sed eos enim exercitationem delectus magni voluptas quis consequatur eaque vitae ut.'),
+(266, 53, 12, '2020-07-19 16:25:17', 'Nisi asperiores accusantium laboriosam in placeat et error quibusdam aperiam et provident dolorem eos hic deleniti.'),
+(267, 53, 8, '2020-07-19 16:25:17', 'Sunt inventore eos qui non at ratione perferendis voluptatem nihil id magni sit.'),
+(268, 53, 11, '2020-07-19 16:25:17', 'Nihil tempora fugiat totam natus nostrum maxime quia quia qui aliquam est dolores sequi.'),
+(269, 53, 8, '2020-07-19 16:25:17', 'Eligendi non excepturi est aperiam animi quasi dolorem eveniet consequatur.'),
+(270, 53, 6, '2020-07-19 16:25:17', 'Aut omnis quia aut nihil minus quia et cupiditate veritatis voluptatem deleniti velit.'),
+(271, 54, 4, '2020-07-19 16:25:17', 'Aut rerum et distinctio est est ea eos ut quae quis veniam aut alias sint.'),
+(273, 54, 10, '2020-07-19 16:25:17', 'Aut consectetur illum totam necessitatibus eius repellendus vero possimus id porro omnis dicta.'),
+(274, 55, 5, '2020-07-19 16:25:17', 'Sit cupiditate quisquam iusto harum et a dolorem in vero amet mollitia.'),
+(275, 55, 9, '2020-07-19 16:25:17', 'Eligendi aliquid ipsa sed rerum accusamus voluptas sunt dolores.'),
+(276, 55, 7, '2020-07-19 16:25:17', 'Culpa omnis accusamus modi dolorem consequuntur perferendis ipsam ipsam.'),
+(277, 56, 8, '2020-07-19 16:25:17', 'Ea hic qui quaerat et quia dolor nihil voluptatibus eum.'),
+(278, 56, 7, '2020-07-19 16:25:17', 'Debitis optio nam rerum minima ut praesentium sunt et iure.'),
+(279, 56, 5, '2020-07-19 16:25:17', 'Similique nesciunt minima voluptas sapiente consequuntur temporibus ipsum odio.'),
+(280, 56, 5, '2020-07-19 16:25:17', 'Mollitia sequi recusandae totam doloribus voluptas repellat velit fugit sed consequatur consequatur possimus quos et corporis.'),
+(281, 56, 12, '2020-07-19 16:25:17', 'Est molestiae doloribus suscipit qui inventore quo at veritatis.'),
+(282, 57, 3, '2020-07-19 16:25:17', 'In dolore rem expedita architecto et et in et voluptatem odit praesentium odio in.'),
+(283, 57, 12, '2020-07-19 16:25:17', 'Et quia quos ut qui distinctio atque qui accusamus repellendus.'),
+(284, 57, 7, '2020-07-19 16:25:17', 'Harum est molestias provident iure iusto doloribus labore officiis.'),
+(285, 57, 6, '2020-07-19 16:25:17', 'Minus corporis et officia accusantium voluptatem excepturi quasi.'),
+(286, 57, 12, '2020-07-19 16:25:17', 'Eveniet similique eius quibusdam nesciunt totam molestiae qui labore officiis error.'),
+(287, 57, 3, '2020-07-19 16:25:17', 'Nemo quisquam doloremque repellendus in et autem sint in qui occaecati ut error in impedit omnis fugiat.'),
+(288, 58, 10, '2020-07-19 16:25:17', 'Et sunt voluptatem dolorem quis qui inventore voluptas earum et in aspernatur aspernatur non.'),
+(289, 58, 6, '2020-07-19 16:25:17', 'Sunt minima mollitia assumenda quaerat mollitia et nisi et in officiis ducimus impedit sed nulla et.'),
+(290, 58, 6, '2020-07-19 16:25:17', 'Odit voluptatum et veniam expedita est deleniti nihil id et sit recusandae omnis.'),
+(291, 58, 8, '2020-07-19 16:25:17', 'Ut accusamus occaecati quam quia repellendus pariatur soluta omnis facere quaerat officia possimus illum.'),
+(292, 58, 5, '2020-07-19 16:25:17', 'Illo beatae mollitia omnis qui voluptatum hic aut hic repellat sint id nihil ipsum autem.'),
+(293, 59, 7, '2020-07-19 16:25:17', 'Et et eius et repudiandae quam laborum ut molestias quam mollitia id ex.'),
+(294, 59, 5, '2020-07-19 16:25:17', 'Ea qui libero iste debitis saepe vitae at dolor et animi.'),
+(295, 59, 10, '2020-07-19 16:25:17', 'Ut distinctio consequatur dolore sint velit deleniti fugit et cumque omnis qui dignissimos dolore omnis.'),
+(296, 59, 9, '2020-07-19 16:25:17', 'Adipisci odit quaerat et quisquam odit natus quisquam voluptas error velit voluptas.'),
+(298, 59, 7, '2020-07-19 16:25:17', 'Voluptas et veritatis quis recusandae dolores nam nobis aliquam consequatur dolorum nam accusamus.'),
+(299, 60, 4, '2020-07-19 16:25:17', 'Libero debitis aut aliquam hic adipisci blanditiis labore deleniti necessitatibus ad id iusto sint mollitia fugiat.'),
+(300, 60, 8, '2020-07-19 16:25:17', 'Fugit et et nesciunt ut ipsa odio et aut quos labore natus praesentium.');
+
+--
+-- Déchargement des données de la table `config`
+--
+
+INSERT INTO `config` (`id`, `nb_tricks_display`, `email_admin`, `nb_messages_display`, `protect_level`) VALUES
+(1, 15, 'gregcodeur@gmail.fr', 10, 0);
+
+--
+-- Déchargement des données de la table `group`
+--
+
+INSERT INTO `group` (`id`, `name`) VALUES
+(1, 'grabs'),
+(2, 'rotation'),
+(3, 'flips'),
+(4, 'slides'),
+(5, 'one foot tricks'),
+(6, 'old school');
+
+--
+-- Déchargement des données de la table `media`
+--
+
+INSERT INTO `media` (`id`, `figure_id`, `url`, `type`, `caption`) VALUES
+(1512, 60, 'https://jimmysweat.ovh/uploads/images/7d7c9bdae614925fb84aae449854c653.jpeg', 0, 'Backside triple cork 1440'),
+(1513, 60, 'https://s.yimg.com/ny/api/res/1.2/mwLymhUGkwjDdSd__sgJBw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/c4db3c848085c6908f4e08245f3bbf9a', 0, 'Triple cork'),
+(1514, 60, 'https://jimmysweat.ovh/uploads/images/2c1a2b480bc54078e04953c10bba755d.jpeg', 0, 'triple cork'),
+(1515, 60, 'https://www.youtube.com/embed/URFnYGzu9lU', 1, 'video Backside triple cork 1440'),
+(1516, 60, 'https://www.youtube.com/embed/2Pi2hoyWS20', 1, 'video snow backside'),
+(1517, 59, 'https://www.youtube.com/embed/_hxLS2ErMiY', 1, 'Method air'),
+(1518, 59, 'https://miro.medium.com/max/1200/1*vV1tWalQFCyMhMPsSFwthA.jpeg', 0, 'Method air James streater'),
+(1519, 59, 'https://i2.wp.com/www.snowboarder.com/wp-content/uploads/2009/09/picture-53.png?ssl=1', 0, 'Method air 1996'),
+(1520, 59, 'https://img.bnqt.com/CMS/bnqt/network/ttrworldtour.bnqt.com/media/ttrwt/003a-42c20dd2-4cd0601f-7924-f3b4d826.jpg', 0, 'method air'),
+(1521, 58, 'https://jimmysweat.ovh/uploads/images/2ed2ead00cb49e7e1bfe3f9705a5a191.jpeg', 0, 'Double Backflip One foot -jimmy sweat'),
+(1522, 58, 'https://www.youtube.com/embed/flvbfuAX1_U', 1, 'video double backflip'),
+(1523, 58, 'https://jimmysweat.ovh/uploads/images/d7ad9333152b513c32b369a0e29f61ac.jpeg', 0, 'Red boul donne des ailes'),
+(1524, 57, 'https://miro.medium.com/max/4096/1*-7dQ2rEHq_aiD-IvzUXzfw.png', 0, 'Mc twist'),
+(1525, 57, 'https://vibrantlifeblog.files.wordpress.com/2010/02/kazuhiro-kokubo-mctwist.jpg', 0, 'Double mc twist'),
+(1526, 57, 'https://www.youtube.com/embed/qIr2ki4nWkU', 1, 'vide mc twist double'),
+(1527, 56, 'https://www.youtube.com/embed/CMWel8h2vho', 1, 'Crippler video'),
+(1528, 56, 'https://www.youtube.com/embed/00kWpAK3I1Y', 1, 'crippler 2'),
+(1529, 56, 'https://i.pinimg.com/originals/c5/9a/4f/c59a4f5625300ba418d6ebecc3b5d0bd.jpg', 0, 'Crippler 900'),
+(1530, 55, 'https://snowtricks.e-mercadal.com/uploads/tricks/frontside-misty-87.jpg', 0, 'Gutterball'),
+(1531, 55, 'https://snowtricks.vincentlescot.fr/public/image/snowtrick/gutterball/gutterball1.jpg', 0, 'gutterball'),
+(1532, 54, 'https://arts-majeurs.com/uploads/images/tricks/22.jpg', 0, 'japan'),
+(1533, 54, 'https://coresites-cdn-adm.imgix.net/whitelines_new/wp-content/uploads/2014/01/Trick-Japan-Grab-620x445.jpg', 0, 'japan'),
+(1534, 54, 'https://www.youtube.com/embed/CzDjM7h_Fwo', 1, 'japan video'),
+(1535, 53, 'https://lh3.googleusercontent.com/proxy/AQMnHhwEzU32mLoQRujbV9DRcLvUVsA_iu5y7JPjL98DWp-fhARtOjpGLf7YbNwOWipzZWfK0y9xDAtO8_13uhknKdOvgxbROdZUdL9EXROt54w9zcpmYxXxmVcSvwZ8A53xRmpjpfY_jCZqxJIWeWSEKg', 0, 'mute'),
+(1536, 53, 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/c/0/b/c0b199d73b_120515_lexique-snowboard.jpg', 0, 'mute cover'),
+(1537, 53, 'https://images.freeimages.com/images/large-previews/617/mute-air-on-snowboard-1438217.jpg', 0, 'mute 2'),
+(1538, 53, 'https://www.youtube.com/embed/CflYbNXZU3Q', 1, 'mute video snow'),
+(1539, 53, 'https://www.slate.com/content/dam/slate/blogs/five_ring_circus/2014/02/08/sage_kotsenburg_slopestyle_the_1620_japan_air_mute_grab_explained/467139965-sage-kotsenburg-competes-in-the-mens-snowboard_1.jpg.CROP.promo-large.jpg', 0, 'mute with japan'),
+(1540, 52, 'https://coresites-cdn-adm.imgix.net/whitelines_new/wp-content/uploads/2014/01/Tricks-Stalefish-Grab-620x393.jpg', 0, 'stalefish'),
+(1541, 52, 'https://i.ytimg.com/vi/f9FjhCt_w2U/sddefault.jpg', 0, 'stalefish 2'),
+(1542, 52, 'https://www.hebergeur-france.net/assets/img/bawbaw-snow.jpg', 0, 'stalefish 3'),
+(1543, 52, 'https://www.youtube.com/embed/xXCCGYqAWqI', 1, 'stalefish video'),
+(1544, 51, 'https://i.pinimg.com/originals/4a/d2/13/4ad213178628922bfa102da16ddd03fe.jpg', 0, 'Rodeo'),
+(1545, 51, 'https://coresites-cdn-adm.imgix.net/whitelines_new/wp-content/uploads/2009/10/Robin-rodeo.jpg', 0, 'Rodea 2'),
+(1546, 51, 'https://www.youtube.com/embed/GXnBhoeYBwY', 1, 'Rodeo snowboard - video');
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `title`) VALUES
+(1, 'ROLE_ADMIN');
+
+--
+-- Déchargement des données de la table `role_user`
+--
+
+INSERT INTO `role_user` (`role_id`, `user_id`) VALUES
+(1, 2);
+
+--
+-- Déchargement des données de la table `tricks`
+--
+
+INSERT INTO `tricks` (`id`, `user_id`, `family_id`, `name`, `description`, `date_add`, `date_modif`, `cover`, `slug`) VALUES
+(51, 12, 2, 'rodeo', 'Le rodeo est une rotation désaxée, qui se reconnaît par son aspect vrillé.', '2019-01-29', '2020-07-22', 'https://i.pinimg.com/originals/4a/d2/13/4ad213178628922bfa102da16ddd03fe.jpg', 'rodeo'),
+(52, 5, 1, 'STALEFISH', 'Saisie de la carre backside de la planche entre les deux pieds avec la main arrière.', '2020-05-25', '2020-07-22', 'https://coresites-cdn-adm.imgix.net/whitelines_new/wp-content/uploads/2014/01/Tricks-Stalefish-Grab-620x393.jpg', 'stalefish'),
+(53, 8, 3, 'Mute', 'La main avant saisit la pointe des pieds entre les orteils ou devant le pied avant. Les variations incluent le Mute Stiffy, dans lequel une prise de silence est effectuée tout en redressant les deux jambes, ou alternativement, certains snowboarders attraperont muet et feront pivoter le bord avant de 90 degrés.', '2018-09-23', '2020-07-22', 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/c/0/b/c0b199d73b_120515_lexique-snowboard.jpg', 'mute'),
+(54, 11, 1, 'Japan', 'La main avant saisit la pointe entre les pieds et le genou avant est tiré vers la planche.', '2018-02-08', '2020-07-22', 'https://arts-majeurs.com/uploads/images/tricks/22.jpg', 'japan'),
+(55, 4, 1, 'Gutterball', 'Le Gutterball est un slide avant à un pied (le pied avant est attaché et le pied arrière est dégagé) avec une pince de ceinture de sécurité arrière, ressemblant à la position du corps après avoir lâché une boule de bowling dans un allié. Cette astuce a été inventée et nommée par Jeremy Cameron, qui lui a valu une première place au concours \"FAME WAR\" de Morrow Snowboards en 2009.', '2020-07-10', '2020-07-22', 'https://snowtricks.vincentlescot.fr/public/image/snowtrick/gutterball/gutterball1.jpg', 'gutterball'),
+(56, 10, 2, 'Crippler', 'Une autre rotation tête en bas classique qui s\'apparente à un backflip sur un mur frontside de pipe ou un quarter.', '2017-12-01', '2020-07-22', 'https://i.pinimg.com/originals/c5/9a/4f/c59a4f5625300ba418d6ebecc3b5d0bd.jpg', 'crippler'),
+(57, 10, 2, 'Double Mc Twist 1260', 'Le Mc Twist est un flip (rotation verticale) agrémenté d\'une vrille. Un saut très périlleux réservé aux professionnels. \r\nLe champion précoce Shaun White s\'est illustré par un Double Mc Twist 1260 lors de sa session de Half-Pipe aux Jeux Olympiques de Vancouver en 2010. Nul doute que c\'est cette figure qui lui a valu de remporter la médaille d\'or', '2015-12-30', '2020-07-22', 'https://vibrantlifeblog.files.wordpress.com/2010/02/kazuhiro-kokubo-mctwist.jpg', 'double-mc-twist-1260'),
+(58, 6, 5, 'Double Backflip One Foot', 'Comme on peut le deviner, les \"one foot\" sont des figures réalisées avec un pied décroché de la fixation. Pendant le saut, le snowboarder doit tendre la jambe du côté dudit pied. L\'esthète Scotty Vine – une sorte de Danny MacAskill du snowboard – en a réalisé un bel exemple avec son Double Backflip One Foot.', '2017-06-26', '2020-07-22', 'https://jimmysweat.ovh/uploads/images/2ed2ead00cb49e7e1bfe3f9705a5a191.jpeg', 'double-backflip-one-foot'),
+(59, 4, 6, 'Method Air', 'Cette figure – qui consiste à attraper sa planche d\'une main et le tourner perpendiculairement au sol – est un classique \"old school\". Il n\'empêche qu\'il est indémodable, avec de vrais ambassadeurs comme Jamie Lynn ou la star Terje Haakonsen. En 2007, ce dernier a même battu le record du monde du \"air\" le plus haut en s\'élevant à 9,8 mètres au-dessus du kick (sommet d\'un mur d\'une rampe ou autre structure de saut).', '2017-01-28', '2020-07-22', 'https://img.bnqt.com/CMS/bnqt/network/ttrworldtour.bnqt.com/media/ttrwt/003a-42c20dd2-4cd0601f-7924-f3b4d826.jpg', 'method-air'),
+(60, 8, 2, 'Backside Triple Cork 1440', 'En langage snowboard, un cork est une rotation horizontale plus ou moins désaxée, selon un mouvement d\'épaules effectué juste au moment du saut. Le tout premier Triple Cork a été plaqué par Mark McMorris en 2011, lequel a récidivé lors des Winter X Games 2012... avant de se faire voler la vedette par Torstein Horgmo, lors de ce même championnat. Le Norvégien réalisa son propre Backside Triple Cork 1440 et obtint la note parfaite de 50/50.', '2020-07-14', '2020-07-22', 'https://jimmysweat.ovh/uploads/images/7d7c9bdae614925fb84aae449854c653.jpeg', 'backside-triple-cork-1440');
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `hash`, `email`, `avatar`, `token`, `description`, `is_validate`) VALUES
+(2, 'admin', '$2y$13$Ka4nYsspLvJ/F2az6sB0QugwPNIa9MUscsHjfK9ivoIJq1EDhsatG', 'gregcodeur@gmail.com', NULL, '5f1473e8e236b', NULL, 1),
+(3, 'user_test', '$2y$13$tRrCDz6QUXY59lXQRM//6uyTHqWgbSMZVl9dIy4GW0LM7GFY3sUq2', 'test@test.fr', 'https://randomuser.me/api/portraits/women/17.jpg', '5f1473e95dfdb', 'Tempora necessitatibus laborum necessitatibus ea.', 1),
+(4, 'Raymond', '$2y$13$OW8x0psrxnoSoLD9tkeDmeL.5HMSm0Hb4RkGkwD7B0ZREFffNsLxq', 'labbe.sebastien@free.fr', 'https://randomuser.me/api/portraits/men/5.jpg', '5f1473e9d5245', 'Quis veritatis laudantium necessitatibus autem voluptatem beatae excepturi.', 1),
+(5, 'Marianne', '$2y$13$3GWrmmKLx.wHJ8B9Z4F0cOEXRb9ji9Qk0N9ZX/C/XUmNSGJN7/M1e', 'jhenry@thomas.com', 'https://randomuser.me/api/portraits/women/76.jpg', '5f1473ea50512', 'Dolorem voluptas et quod vitae voluptatibus.', 1),
+(6, 'Margaux', '$2y$13$fk/J0GL/XqkfDLs1FMVWnuHkvzHvsICq3ni.vAp44NrUtX2uGAW/6', 'emile10@morvan.net', 'https://randomuser.me/api/portraits/women/16.jpg', '5f1473eabff68', 'Exercitationem nihil voluptatem necessitatibus necessitatibus et voluptatem.', 1),
+(7, 'Nathalie', '$2y$13$28vnUHDbgEKLYz830YZRuugoT1AsAM9a1BgG0zUipxweyNmuTiYFS', 'rbegue@fernandez.com', 'https://randomuser.me/api/portraits/women/55.jpg', '5f1473eb3b99c', 'Ducimus ut quia impedit eius et.', 1),
+(8, 'Georges', '$2y$13$UpI9hip1KmWGsjCwhLTU5.GHRvn7ThzGRaS1inPhrpYzlNexf7l1a', 'pereira.luc@wanadoo.fr', 'https://randomuser.me/api/portraits/men/64.jpg', '5f1473ebab3e0', 'Odio eum dolores voluptatem similique et.', 1),
+(9, 'Jérôme', '$2y$13$pnZxj/MTpMANRXhC9RaVOOXzbLBxukBftbyIV5rf8iXyvF6Q1qjpW', 'urobin@thierry.com', 'https://randomuser.me/api/portraits/men/70.jpg', '5f1473ec27fe4', 'Cumque nostrum harum qui nam eos in.', 1),
+(10, 'Margaret', '$2y$13$JP905qTCv0OiKzOZbezp2uunDaprJTC6IFYlHKFbWM6qV8UTi9PuC', 'pires.theophile@schneider.org', 'https://randomuser.me/api/portraits/women/10.jpg', '5f1473ec9840c', 'Ab dolorum aut sed modi animi sed non.', 1),
+(11, 'Emmanuelle', '$2y$13$xxg4sIaG/p5QvKYXHNZhiukj2gbodI41Gi4claDc51CbOW2COogyy', 'robert.alexandre@bigot.fr', 'https://randomuser.me/api/portraits/women/85.jpg', '5f1473ed1419e', 'Distinctio dolores sed et placeat similique eum.', 1),
+(12, 'Gilles', '$2y$13$SaMUKD83cQf21IVarKMLH.zRp7dSAFXGpzpmL5MwkuH4kq2Gs2g9m', 'sebastien.klein@dbmail.com', 'https://randomuser.me/api/portraits/men/37.jpg', '5f1473ed85015', 'Inventore corrupti non nostrum saepe ab ipsa.', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
